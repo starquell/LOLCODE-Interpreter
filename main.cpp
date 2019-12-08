6c5a7f58744d3ae412d6529021d6d25ad5728ff4
Ilya <illia.gide@gmail.com>
2019-12-08 14:06:29 +0200

    base types impl

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "src/Parser.hpp"
+#include <iostream>
+
+
+int main () {
+
+     parser::parse_str(R"(325235325 , 23323.1, 23232, 32, "Hello fren", 31244, "ou", 23.2, WIN)");
+   // std::cout << result << std::endl;
+    return 0;
+}
---
+++ src/BaseTypes.hpp
@@ -0,0 +1,14 @@
+#ifndef LOLCODE_BASETYPES_HPP
+#define LOLCODE_BASETYPES_HPP
+
+#include <string>
+
+namespace type {
+   using Integer = int;
+   using Floating = double;
+   using String = std::string;
+   using Boolean = bool;
+
+   class Null {};
+}
+#endif //LOLCODE_BASETYPES_HPP
